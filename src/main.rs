3e60acfd7e3017c50e254f038577650b66f0ebb7
AnythingJoes <101760500+AnythingJoes@users.noreply.github.com>
2022-05-14 22:08:33 -0400

    Read instruction A2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut byte_vec = fs::read("./tictactoe.bin").unwrap();
+    let mut bytes = byte_vec.iter_mut();
+
+    loop {
+        let byte = bytes.next().expect("End of program");
+        match byte {
+            0xA2 => {
+                println!("Got A2!")
+            }
+            instruction => {
+                panic!("Unkown instruction: {:X}", instruction);
+            }
+        }
+    }
 }
