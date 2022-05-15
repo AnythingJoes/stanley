ea92acf9fb6a5ac315aab0cbba2a310f7dc25e9d
AnythingJoes <101760500+AnythingJoes@users.noreply.github.com>
2022-05-14 22:08:33 -0400

    Implement TIA Set for Playfield

--- src/system/mod.rs
+++ src/system/mod.rs
@@ -12,6 +12,7 @@ const PROGRAM_SIZE: usize = 0x1FFF - 0x1000 + 1;
 pub struct System {
     pub chip: Nmos6507,
     pub riot: Riot,
+    pub tia: Tia,
     pub memory: [u8; MEMORY_SIZE],
     pub program: [u8; PROGRAM_SIZE],
     // TODO temporarily track clocks
@@ -23,6 +24,7 @@ impl System {
         Self {
             chip: Nmos6507::new(),
             riot: Riot::default(),
+            tia: Tia::default(),
             clocks: 0,
             memory: [0; MEMORY_SIZE],
             program,
@@ -41,8 +43,7 @@ impl System {
 
         // // TIA
         if (!index & 0x1080) == 0x1080 {
-            //     return &mut self.tia[(index & 0x003F) as usize];
-            return;
+            return self.tia.set(index & 0x003F, value);
         }
 
         // RIOT
@@ -66,22 +67,11 @@ impl System {
         }
 
         // TIA Read
-        // TODO: Needs a real implementation
-        // If it ends in 0xC, it's trying to read player 0 input in this case 0
-        // is pressed and 1 in the sign bit is the default state. We want to
-        // return the default state until we implement input
         if (!index & 0x1080) == 0x1080 {
-            if (index & 0x000F) == 0xC {
-                return 0b1000_0000;
-            }
-            return 0;
+            return self.tia.get(index & 0x000F);
         }
 
-        // RIOT Read
         if (!index & 0x1000) == 0x1000 && (index & 0x0480) != 0 {
-            // INTIM timer check
-            // if index & 0x0284 == 0x0284 { return &1; // anything but zero
-            // }
             return self.riot.get(index);
         }
 
@@ -124,6 +114,61 @@ RAM\r\n",
             write!(f, "\r\n")?;
         }
         Ok(())
+    }
+}
+
+#[derive(Default)]
+pub struct Tia {
+    vsync: bool,
+    vblank: bool,
+    wsync: bool,
+    // colors
+    colupf: u8,
+    colubk: u8,
+
+    //ctrlpf
+    pf_reflected: bool,
+
+    //pf registers
+    pf0: u8,
+    pf1: u8,
+    pf2: u8,
+}
+
+impl Tia {
+    fn set(&mut self, index: u16, value: u8) {
+        match index {
+            0x00 => self.vsync = (value & 0x02) != 0,
+            // TODO: vblank does other thing on D6 and D7 pins, will need to be implemented
+            0x01 => self.vblank = (value & 0x02) != 0,
+            0x02 => self.wsync = true,
+            // TODO: RSYNC: can be ignored in most cases. There is one game that depends on this being
+            // handled correctly
+            0x03 => (),
+            0x04..=0x07 => (), // Ignored for now
+            0x08 => self.colupf = value,
+            0x09 => self.colubk = value,
+            // TODO: other parts of ctrlpf
+            0x0A => self.pf_reflected = (value & 0x01) == 1,
+            0x0B..=0x0C => (), // Ignored for now
+            0x0D => self.pf0 = value & 0xF0,
+            0x0E => self.pf1 = value,
+            0x0F => self.pf2 = value,
+            0x10..=0x2C => (), // Ignored for now
+            0x2D..=0x3F => (), // Unused
+            _ => unreachable!("Tia set not implemented for {:04X} index", index),
+        }
+    }
+
+    fn get(&self, index: u16) -> u8 {
+        // TODO: Needs a real implementation
+        // If it ends in 0xC, it's trying to read player 0 input in this case 0
+        // is pressed and 1 in the sign bit is the default state. We want to
+        // return the default state until we implement input
+        if (index & 0x000F) == 0xC {
+            return 0b1000_0000;
+        }
+        unimplemented!("Tia get not implemented for {:04X} index", index);
     }
 }
 
