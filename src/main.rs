a9e3f3f7026d4daa061c5ea380de3ec22090b176
AnythingJoes <101760500+AnythingJoes@users.noreply.github.com>
2022-05-14 22:08:33 -0400

    Handle A2 Insruction

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 use std::fs;
 
+#[derive(Debug, Default)]
+struct Nmos6502 {
+    x: u8,
+}
+
 fn main() {
     let mut byte_vec = fs::read("./tictactoe.bin").unwrap();
     let mut bytes = byte_vec.iter_mut();
+    let mut chip = Nmos6502::default();
 
     loop {
         let byte = bytes.next().expect("End of program");
         match byte {
             0xA2 => {
-                println!("Got A2!")
+                // Mode: Immediate
+                // Syntax: LDX #$44
+                // Hex: $A2
+                // Width: 2
+                // Timing: 2
+                let arg = bytes.next().expect("Expected arg for 0xA2 got end of file");
+                chip.x = *arg;
             }
             instruction => {
                 panic!("Unkown instruction: {:X}", instruction);
