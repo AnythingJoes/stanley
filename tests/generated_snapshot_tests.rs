bf52ae01936a12ced8f362fe024017fbe0ea6167
AnythingJoes <101760500+AnythingJoes@users.noreply.github.com>
2022-06-28 13:52:51 -0400

    Clean up gnarly for loops into iterator a la esitsu

--- tests/generated_snapshot_tests.rs
+++ tests/generated_snapshot_tests.rs
@@ -9,9 +9,11 @@ use stanley::system::tia::{BUFF_SIZE, HEIGHT, STRIDE, WIDTH};
 use stanley::system::{instructions::Instruction, System};
 use stanley::Result;
 
-const OUTPUT_WIDTH: usize = (WIDTH * 6) as usize;
-const OUTPUT_HEIGHT: usize = (HEIGHT * 4) as usize;
+const OUTPUT_WIDTH: usize = WIDTH as usize * OUTPUT_PIXEL_WIDTH;
+const OUTPUT_HEIGHT: usize = HEIGHT as usize * OUTPUT_PIXEL_HEIGHT;
 const OUTPUT_STRIDE: usize = 3;
+const OUTPUT_PIXEL_HEIGHT: usize = 4;
+const OUTPUT_PIXEL_WIDTH: usize = 6;
 
 include!(concat!(env!("OUT_DIR"), "/tests.rs"));
 
@@ -83,67 +85,63 @@ fn test_snapshot(snapshot_path: impl AsRef<Path>) {
         }
     }
 
-    let mut differences = screen
-        .chunks(STRIDE)
-        .zip(screen_actual.chunks(STRIDE))
-        .enumerate()
-        .filter_map(|(i, (expected, actual))| {
-            if expected == actual {
-                None
-            } else {
-                Some((i, actual))
-            }
-        })
-        .peekable();
-    let any_differences = differences.peek().is_some();
-
+    let any_differences = screen != screen_actual;
     if any_differences {
-        let mut buffer: Vec<Vec<[u8; 3]>> =
-            vec![vec![[0; OUTPUT_STRIDE]; OUTPUT_WIDTH]; OUTPUT_HEIGHT];
-
-        let (mut i, mut actual) = differences.next().unwrap();
-        let border_pixel = [0xFF, 0x00, 0x00];
-
-        for row in 0..HEIGHT as usize {
-            for column in 0..WIDTH as usize {
-                let location = row * WIDTH as usize + column;
-                let screen_pixel = &screen[location * STRIDE..location * STRIDE + STRIDE];
-                let expected_pixel = [screen_pixel[2], screen_pixel[1], screen_pixel[0]];
-                let output_row_start = row * 4;
-                let output_column_start = column * 6;
-
-                if i == location {
-                    let actual_out_pixel = [actual[2], actual[1], actual[0]];
-                    for output_row in output_row_start..output_row_start + 4 {
-                        for output_column in output_column_start..output_column_start + 6 {
-                            let out_pixel = if output_row == output_row_start
-                                || output_row == output_row_start + 3
-                                || output_column == output_column_start
-                                || output_column == output_column_start + 5
-                            {
-                                border_pixel
-                            } else if output_column - output_column_start < 3 {
-                                expected_pixel
-                            } else {
-                                actual_out_pixel
-                            };
-                            buffer[output_row][output_column] = out_pixel;
-                        }
-                    }
-                    if let Some((idx, a)) = differences.next() {
-                        i = idx;
-                        actual = a;
-                    }
+        let buffer = screen
+            .chunks(STRIDE)
+            .zip(screen_actual.chunks(STRIDE))
+            .map(|(expected, actual)| {
+                (
+                    [expected[2], expected[1], expected[0]],
+                    [actual[2], actual[1], actual[0]],
+                )
+            })
+            .map(|(expected, actual)| {
+                if expected == actual {
+                    let row = [expected; OUTPUT_PIXEL_WIDTH];
+                    [row; OUTPUT_PIXEL_HEIGHT]
                 } else {
-                    for output_row in output_row_start..output_row_start + 4 {
-                        for output_column in output_column_start..output_column_start + 6 {
-                            buffer[output_row][output_column] = expected_pixel;
-                        }
+                    let mut row = [[0; OUTPUT_STRIDE]; OUTPUT_PIXEL_WIDTH];
+                    let mut output_pixel = [row; OUTPUT_PIXEL_HEIGHT];
+                    let red = [0xFF, 0x00, 0x00];
+
+                    row[0] = red;
+                    let diff_pixel_width = (OUTPUT_PIXEL_WIDTH - 2) / 2;
+                    for item in row.iter_mut().skip(1).take(diff_pixel_width) {
+                        *item = expected;
                     }
+                    for item in row
+                        .iter_mut()
+                        .skip(1 + diff_pixel_width)
+                        .take(diff_pixel_width)
+                    {
+                        *item = actual;
+                    }
+                    row[row.len() - 1] = red;
+                    output_pixel[0] = [red; OUTPUT_PIXEL_WIDTH];
+                    for item in output_pixel
+                        .iter_mut()
+                        .skip(1)
+                        .take(OUTPUT_PIXEL_HEIGHT - 2)
+                    {
+                        *item = row;
+                    }
+                    output_pixel[output_pixel.len() - 1] = [red; OUTPUT_PIXEL_WIDTH];
+                    output_pixel
                 }
-            }
-        }
-        create_ppm(buffer, test_name).unwrap();
+            })
+            .collect::<Vec<_>>()
+            .chunks(WIDTH as usize)
+            .flat_map(|row| {
+                let mut output: Vec<Vec<[u8; OUTPUT_STRIDE]>> = vec![vec![]; OUTPUT_PIXEL_HEIGHT];
+                for i in 0..output.len() {
+                    output.push(row.iter().flat_map(|pixel| pixel[i]).collect());
+                }
+                output.into_iter().flatten()
+            })
+            .flatten()
+            .collect::<Vec<u8>>();
+        create_ppm(&buffer, test_name).unwrap();
     }
 
     assert!(
@@ -153,7 +151,7 @@ fn test_snapshot(snapshot_path: impl AsRef<Path>) {
     )
 }
 
-fn create_ppm(buffer: Vec<Vec<[u8; 3]>>, output_name: &str) -> Result<()> {
+fn create_ppm(buffer: &[u8], output_name: &str) -> Result<()> {
     fs::create_dir_all("./artifacts")?;
 
     let mut ppm_image = fs::File::create(format!("./artifacts/{output_name}.ppm"))?;
@@ -161,10 +159,6 @@ fn create_ppm(buffer: Vec<Vec<[u8; 3]>>, output_name: &str) -> Result<()> {
     writeln!(ppm_image, "{} {}", OUTPUT_WIDTH, OUTPUT_HEIGHT)?;
     writeln!(ppm_image, "255")?;
 
-    for line in buffer {
-        for pixel in line {
-            ppm_image.write_all(&pixel)?;
-        }
-    }
+    ppm_image.write_all(buffer)?;
     Ok(())
 }
